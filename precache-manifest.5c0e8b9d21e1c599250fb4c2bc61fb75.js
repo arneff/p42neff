97f44825e00108787c760f17ffac7a31bda94de5
Aaron Neff <aaron.neff@stackhawk.com>
2022-02-08 10:31:49 -0600

    Updates

---
+++ precache-manifest.5c0e8b9d21e1c599250fb4c2bc61fb75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e86500c4bcfd33c7f00c93fc969c80",
+    "url": "/p42neff/index.html"
+  },
+  {
+    "revision": "abf1d4f1a90dd43360f2",
+    "url": "/p42neff/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "63e1e78efa5c70985a52",
+    "url": "/p42neff/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "abf1d4f1a90dd43360f2",
+    "url": "/p42neff/static/js/2.1a856f2d.chunk.js"
+  },
+  {
+    "revision": "24cba0c88f46204e4e9505858b825c5d",
+    "url": "/p42neff/static/js/2.1a856f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e1e78efa5c70985a52",
+    "url": "/p42neff/static/js/main.d2439fbd.chunk.js"
+  },
+  {
+    "revision": "c546450ce8fa0d741ca5",
+    "url": "/p42neff/static/js/runtime-main.7e2e65e3.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/p42neff/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "dabc05847775c756c1c271293ea3371a",
+    "url": "/p42neff/static/media/pizza.dabc0584.png"
+  },
+  {
+    "revision": "3ec930d230b1018f15e005cc35863d33",
+    "url": "/p42neff/static/media/whole.3ec930d2.png"
+  }
+]);
